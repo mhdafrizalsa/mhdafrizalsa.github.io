fc7f36036efa818f32e6b6d8c40eaf2ea46466f9
mhdafrizalsa <mhdafrizalsyafaatulamri@gmail.com>
2020-08-15 14:25:26 +0700

    Update

--- js/main.js
+++ js/main.js
@@ -95,10 +95,12 @@ $.ajax({
     type: 'GET',
     success: function(data) {  
       if (data.length > 0) { 
-        $.each(data.reverse(), function(i, val) {                
-          $('#shots').prepend(
-            '<a class="shot" target="_blank" href="'+ val.html_url +'" title="' + val.title + '"><div class="title">' + val.title + '</div><img src="'+ val.images.hidpi +'"/></a>'
-            )
+        $.each(data.reverse(), function(i, val) {
+          if (i>2) {
+            $('#shots').prepend(
+              '<a class="shot" target="_blank" href="'+ val.html_url +'" title="' + val.title + '"><div class="title">' + val.title + '</div><img src="'+ val.images.hidpi +'"/></a>'
+            );
+          }
         })
       }
       else {
